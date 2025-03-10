ffa1a3a92b4bc9023acda13f7e5c570fbbb880f7
heyManNice <3031978990@qq.com>
2025-03-10 17:43:31 +0800

    build:构建工具生成build文件夹

--- doc/build.js
+++ doc/build.js
@@ -2,67 +2,59 @@ import fs from 'fs';
 
 class Generator {
     /**
-     * 屏蔽的文件
+     * 目录
      */
-    filterFiles = [
-        'node_modules',
-        'dist',
-        'build',
-        'lib',
-        'public',
-        'src',
-        'test',
-        'docs',
-        '.git',
-        '.idea',
-        '.vscode',
-        '.DS_Store',
-        '.gitignore',
-        '.gitattributes',
-        '.gitmodules',
-        'data.js',
-        'generateData.js',
-        'package.json',
-        'package-lock.json',
-        '.github',
-        'index.html',
-        'README.md',
-    ];
+    docDir = './doc';
+    buildDir = this.docDir+'/build';
+    srcDir = './src';
 
     /**
-     * 获取过滤后的目标文件数据
+     * 过滤文件夹
      * 
      * @returns {string[]}
      * 文件名列表数组
      */
-    getFiles() {
-        let files = fs.readdirSync('./');
+    getSrcFiles() {
+        let files = fs.readdirSync(this.srcDir);
+        //过滤目录
         let filtedFiles = files.filter((file) => {
-            return !this.filterFiles.includes(file); 
+            return fs.statSync(this.srcDir+"/"+file).isFile(); 
         })
+        this.copyDirsFiles();
         return filtedFiles;
     }
 
     /**
-     * 写入data文件
-     * 
+     * 复制源码文件夹到构建文件夹下
+     * 复制主页文件夹到构建文件夹下
+     */
+    copyDirsFiles() {
+        fs.cpSync(this.srcDir, this.buildDir, {recursive: true});
+        fs.copyFileSync(this.docDir+'/index.html', this.buildDir+'/index.html');
+    }
+
+    /**
+     * 更换主页文件中特定变量的数据
      * @param {string[]} files
      * 文件名列表数组
      */
-    writeData(files) {
-        let data = `var data = ${JSON.stringify(files)};`;
-        fs.writeFileSync('./data.js', data); 
+    writeData2Html(files) {
+        const data = `const Project_Files_Data = ${JSON.stringify(files)};`;
+        let index = fs.readFileSync(this.buildDir+'/index.html', 'utf-8');
+        index = index.replace('const Project_Files_Data = [];', data);
+        fs.writeFileSync(this.buildDir+'/index.html', index);
     }
-
     /**
      * 开始生成数据
      * 将当前目录下的目标文件名写入data.js文件
      */
     generate() {
-        let files = this.getFiles();
-        this.writeData(files);
+        this.copyDirsFiles();
+        const files = this.getSrcFiles();
+        this.writeData2Html(files);
     }
 }
 
-let generator = new Generator();
+const generator = new Generator();
 generator.generate();
+console.log('success');
